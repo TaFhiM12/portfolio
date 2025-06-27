41a151558cd9fb25386e8708ff07d307f1513ce9
Tanvir Mahtab Tafhim <tafhim000001@gmail.com>
2025-06-27 22:50:45 +0600

    portfolio environment setup

---
+++ src/MainLayout/MainLayout.jsx
@@ -0,0 +1,16 @@
+import Navbar from "../Components/Navbar";
+
+const MainLayout = () => {
+    return (
+        <div>
+            <header>
+                <Navbar/>
+            </header>
+            <main className="w-11/12 mx-auto p-4">
+                
+            </main>
+        </div>
+    );
+};
+
+export default MainLayout;
